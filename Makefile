bb03441832a645a2ec3767ef759d70c3e8b4bbf4
jwshin <jwshin@jwshin.com>
2023-02-22 10:50:15 -0800

    adding basic tests

--- Makefile
+++ Makefile
@@ -1,12 +1,18 @@
-.PHONY: run memgraph spotless up down build
+.PHONY: run memgraph spotless up down build clean test
 
 run:
 	./gradlew run
 
+clean:
+	./gradlew clean 
+
+check:
+	./gradlew check
+
 memgraph:
 	docker run -it -p 7687:7687 -p 7444:7444 -p 3000:3000 -v mg_lib:/var/lib/memgraph --env MEMGRAPH="--bolt-server-name-for-init=Neo4j/" memgraph/memgraph-platform
 
-spotless:
+format:
 	./gradlew spotlessApply
 
 up:
--- src/test/java/graph/GraphTest.java
+++ src/test/java/graph/GraphTest.java
@@ -1,9 +1,17 @@
 package graph;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import io.micronaut.http.HttpRequest;
+import io.micronaut.http.HttpResponse;
+import io.micronaut.http.HttpStatus;
+import io.micronaut.http.client.BlockingHttpClient;
+import io.micronaut.http.client.HttpClient;
+import io.micronaut.http.client.annotation.Client;
 import io.micronaut.runtime.EmbeddedApplication;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
 import jakarta.inject.Inject;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 @MicronautTest
@@ -11,8 +19,22 @@ class GraphTest {
 
     @Inject EmbeddedApplication<?> application;
 
+    @Inject
+    @Client("/")
+    HttpClient httpClient;
+
     @Test
-    void testItWorks() {
-        Assertions.assertTrue(application.isRunning());
+    void testApplicationStatus() {
+        assertTrue(application.isRunning());
+    }
+
+    @Test
+    void testIndexHttpClient() {
+        BlockingHttpClient client = httpClient.toBlocking();
+        HttpRequest<Object> request = HttpRequest.GET("/");
+        HttpResponse<String> rsp = client.exchange(request, String.class);
+
+        assertEquals(HttpStatus.OK, rsp.getStatus());
+        assertTrue(rsp.getBody().orElse("").startsWith("<!doctype html>"));
     }
 }
